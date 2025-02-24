05cee14449c5376a800b1308fedf7ba0dd997207
hstoykova <hristina.pencheva@gmail.com>
2025-02-24 11:00:12 +0200

    Create 09.fruit.js

---
+++ JS Syntax Fundamentals/09.fruit.js
@@ -0,0 +1,12 @@
+function solve(fruit, grams, price) {
+  let money = (grams / 1000) * price;
+
+  console.log(
+    `I need $${money.toFixed(2)} to buy ${(grams / 1000).toFixed(
+      2
+    )} kilograms ${fruit}.`
+  );
+}
+
+solve("orange", 2500, 1.8);
+solve("apple", 1563, 2.35);
