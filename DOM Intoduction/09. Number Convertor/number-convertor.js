10f17f278ab0e23f6b61da4a7a0d5abe51e963cf
hstoykova <hristina.pencheva@gmail.com>
2025-03-29 18:48:59 +0200

    Created 09. Number Convertor

---
+++ DOM Intoduction/09. Number Convertor/number-convertor.js
@@ -0,0 +1,13 @@
+function solve() {
+  const number = Number(document.getElementById("input").value);
+  const convertTo = document.getElementById("selectMenuTo").value;
+  let result = "";
+
+  if (convertTo === "binary") {
+    result = number.toString(2);
+  } else if (convertTo === "hexadecimal") {
+    result = number.toString(16).toUpperCase();
+  }
+
+  document.getElementById("result").value = result;
+}
