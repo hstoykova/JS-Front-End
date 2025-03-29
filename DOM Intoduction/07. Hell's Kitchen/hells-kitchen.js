6f3ebe6c83dc0c94ad915f1e27ced5d6d902b40c
hstoykova <hristina.pencheva@gmail.com>
2025-03-29 20:49:33 +0200

    Created 07. Hells Kitchen

--- DOM Intoduction/07. Hell's Kitchen/hells-kitchen.js
+++ DOM Intoduction/07. Hell's Kitchen/hells-kitchen.js
@@ -1,3 +1,41 @@
 function solve() {
-    //TODO
+  let data = document.querySelector("#inputs textarea").value;
+  let dataArr = JSON.parse(data);
+  let restaurantMap = new Map();
+
+  for (let element of dataArr) {
+    let [restaurant, workers] = element.split(" - ");
+
+    if (!restaurantMap.has(restaurant)) {
+      restaurantMap.set(restaurant, []);
+    }
+
+    let workerArr = workers.split(", ");
+
+    for (let worker of workerArr) {
+      let [name, wage] = worker.split(" ");
+      restaurantMap.get(restaurant).push({ name, wage: Number(wage) });
+    }
+  }
+
+  let bestRestaurantEntry = [...restaurantMap.entries()]
+    .map(([name, workers]) => {
+      let avgSalary =
+        workers.reduce((acc, curr) => acc + curr.wage, 0) / workers.length;
+      let bestSalary = Math.max(...workers.map((w) => w.wage));
+      return { name, avgSalary, bestSalary, workers };
+    })
+    .sort((a, b) => b.avgSalary - a.avgSalary)[0];
+
+  bestRestaurantEntry.workers.sort((a, b) => b.wage - a.wage);
+
+  document.querySelector("#bestRestaurant p").textContent = `Name: ${
+    bestRestaurantEntry.name
+  } Average Salary: ${bestRestaurantEntry.avgSalary.toFixed(
+    2
+  )} Best Salary: ${bestRestaurantEntry.bestSalary.toFixed(2)}`;
+
+  document.querySelector("#workers p").textContent = bestRestaurantEntry.workers
+    .map((w) => `Name: ${w.name} With Salary: ${w.wage}`)
+    .join(" ");
 }
